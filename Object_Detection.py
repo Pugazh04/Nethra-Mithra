1bd7b75d44c909ea024fd67da4f1a5b87b7f8660
Pugazh04 <115567169+Pugazh04@users.noreply.github.com>
2025-07-08 03:50:02 +0530

    Update Object_Detection.py

--- Object_Detection.py
+++ Object_Detection.py
@@ -4,27 +4,21 @@ import numpy as np
 from ultralytics import YOLO
 from collections import Counter
 
-
-# Load YOLOv8 model
-model = YOLO("yolov8m.pt")  # or yolov8l.pt for best accuracy
+model = YOLO("yolov8m.pt")
 conf_threshold = 0.4
 
 def detect_objects():
     try:
-        # Fetch image from ESP32-CAM
-        esp32_url = "http://192.168.4.1/capture"
+        esp32_url = "<add your ESP32 cam url>"
         response = requests.get(esp32_url, timeout=5)
         if response.status_code != 200:
             print("Failed to fetch image from ESP32-CAM")
             return []
 
-        # Convert image to OpenCV format
         img_array = np.frombuffer(response.content, np.uint8)
         frame = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-        # Optional image enhancement
         frame = cv2.convertScaleAbs(frame, alpha=1.3, beta=20)
 
-        # Run YOLOv8 on the image
         results = model(frame, verbose=False)
         detected_objects = []
         for result in results:
@@ -39,7 +33,6 @@ def detect_objects():
                     "label": class_name,
                     "bbox": (x1, y1, x2, y2)
                 })
-        # Optional: Display annotated frame
         annotated_frame = results[0].plot()
         cv2.imshow("ESP32-CAM YOLOv8 Detection", annotated_frame)
         cv2.imwrite("debug_frame.jpg", annotated_frame)
